b99afd68410ae0842ab09c3b7bd5f0a1b64477fd
SLDem <slavko.dem@gmail.com>
2021-03-28 17:21:53 +0300

    Added email support

---
+++ StockDashboardAPI/rabbit_channel.py
@@ -0,0 +1,21 @@
+import pika
+from StockDashboardAPI import app
+
+
+def connect_queue():
+    rabbitmq = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    return rabbitmq
+
+
+def get_email_queue():
+    connect = connect_queue()
+    channel = connect.channel()
+    channel.queue_declare(queue='email_queue', durable=True)
+    channel.queue_bind(exchange='amq.direct', queue='email_queue')
+    email_queue = channel
+    return email_queue
+
+
+@app.teardown_appcontext
+def close_queue(error, rabbitmq):
+    rabbitmq.close()
