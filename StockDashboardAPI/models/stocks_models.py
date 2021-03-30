db5ac32753f52f7b3453783efa1aad6847e59607
Nevmerzhitsky Yura <nevmerzhitsky.jyra@gmail.com>
2021-03-30 10:39:16 +0300

    updated stocks_models

--- StockDashboardAPI/models/stocks_models.py
+++ StockDashboardAPI/models/stocks_models.py
@@ -1,43 +1,47 @@
 from StockDashboardAPI.utils import pool as db
 
+class Stocks:
+    def __init__(self):
+        self.name = str()
+        self.company_name = str()
 
-def create(name,company_name):
-    with db.Connection() as conn:
-        cursor = conn.cursor()
-        query = f"INSERT INTO public.stocks (name, company_name) VALUES ('%s', '%s');"%(name, company_name)
-        cursor.execute(query)
-        conn.commit()
-        cursor.close()
-
-def update(id,name=None,company_name=None):
-    if not name:
+    def create(self, name,company_name):
         with db.Connection() as conn:
             cursor = conn.cursor()
-            query = f"UPDATE public.stocks SET company_name = '%s' WHERE id = %s;"%(company_name, id)
-            cursor.execute(query)
-            conn.commit()
-            cursor.close()
-    if not company_name:
-        with db.Connection() as conn:
-            cursor = conn.cursor()
-            query = f"UPDATE public.stocks SET name = '%s' WHERE id = %s;"%(name, id)
+            query = f"INSERT INTO public.stocks (name, company_name) VALUES ('%s', '%s');"%(name, company_name)
             cursor.execute(query)
             conn.commit()
             cursor.close()
 
-def remove(id):
-    with db.Connection() as conn:
-        cursor = conn.cursor()
-        query = f"DELETE FROM public.stocks WHERE id = %s;" % (id)
-        cursor.execute(query)
-        conn.commit()
-        cursor.close()
+    def update(self,id,name=None,company_name=None):
+        if not name:
+            with db.Connection() as conn:
+                cursor = conn.cursor()
+                query = f"UPDATE public.stocks SET company_name = '%s' WHERE id = %s;"%(company_name, id)
+                cursor.execute(query)
+                conn.commit()
+                cursor.close()
+        if not company_name:
+            with db.Connection() as conn:
+                cursor = conn.cursor()
+                query = f"UPDATE public.stocks SET name = '%s' WHERE id = %s;"%(name, id)
+                cursor.execute(query)
+                conn.commit()
+                cursor.close()
 
-def get_by_id(id):
-    with db.Connection() as conn:
-        cursor = conn.cursor()
-        query = f"SELECT name, company_name FROM public.stocks WHERE id = %s;" % (id)
-        cursor.execute(query)
-        record = cursor.fetchall()
-        print(record)
-        cursor.close()
+    def remove(self, id):
+        with db.Connection() as conn:
+            cursor = conn.cursor()
+            query = f"DELETE FROM public.stocks WHERE id = %s;" % (id)
+            cursor.execute(query)
+            conn.commit()
+            cursor.close()
+
+    def get_by_id(self, id):
+        with db.Connection() as conn:
+            cursor = conn.cursor()
+            query = f"SELECT name, company_name FROM public.stocks WHERE id = %s;" % (id)
+            cursor.execute(query)
+            record = cursor.fetchall()
+            print(record)
+            cursor.close()
