d2c9eec41eca7c9248ee76f1e4f57f8fdf74a49a
SLDem <slavko.dem@gmail.com>
2021-03-30 17:12:52 +0300

    Modified gitignore and config

--- stock_dashboard_api/config.py
+++ stock_dashboard_api/config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-project_folder = os.path.expanduser('~/StockDashboard/stock_dashboard_api')  # adjust as appropriate
+project_folder = os.path.expanduser('~/StockDashboard/stock_dashboard_api')
 load_dotenv(os.path.join(project_folder, '.env'))
 
 
@@ -11,11 +11,6 @@ class Config:
     CSRF_ENABLED = True
     RABBITMQ_CONNECTION_HOST = os.getenv('RABBITMQ_CONNECTION_HOST')
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
-    MAIL_HOST = os.getenv('MAIL_HOST')
-    MAIL_PORT = os.getenv('MAIL_PORT')
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS')
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 
 
 class ProductionConfig(Config):
