3510a47fdaba27ebb72c8fe3c3ab721d1e541e1a
Nevmerzhitsky Yura <nevmerzhitsky.jyra@gmail.com>
2021-03-31 10:45:50 +0300

    Refactored stock_models

--- stock_dashboard_api/models/stocks_models.py
+++ stock_dashboard_api/models/stocks_models.py
@@ -12,14 +12,11 @@ class Stocks:
     @classmethod
     def create(cls, name, company_name):
         with db.Connection() as conn:
-            cursor = conn.cursor()
             query = f"""INSERT INTO {cls.__table} (name, company_name)
                         VALUES ('{name}', '{company_name}')
                         RETURNING id, name, company_name;"""
-            cursor.execute(query)
-            pk, name, company_name = cursor.fetchone()
-            conn.commit()
-            cursor.close()
+            conn.cursor.execute(query)
+            pk, name, company_name = conn.cursor.fetchone()
             return Stocks(pk=pk, name=name, company_name=company_name)
 
     def update(self, name=None, company_name=None):
@@ -31,29 +28,21 @@ class Stocks:
             query += f"company_name = {company_name} "
         query += f"WHERE id = {self.pk} RETURNING id, name, company_name; "
         with db.Connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute(query)
-            _pk, _name, _company_name = cursor.fetchone()
-            self.name = _name
-            self.company_name = _company_name
-            conn.commit()
-            cursor.close()
+            conn.cursor.execute(query)
+            pk, name, company_name = conn.cursor.fetchone()
+            self.name = name
+            self.company_name = company_name
 
     @classmethod
     def delete_by_id(cls, pk):
         with db.Connection() as conn:
-            cursor = conn.cursor()
-            query = f"DELETE FROM {cls.__table} WHERE id = {pk};"
-            cursor.execute(query)
-            conn.commit()
-            cursor.close()
+            conn.query = f"DELETE FROM {cls.__table} WHERE id = {pk};"
+            conn.cursor.execute(query)
 
     @classmethod
     def get_by_id(cls, pk):
         with db.Connection() as conn:
-            cursor = conn.cursor()
             query = f"SELECT id, name, company_name FROM {cls.__table} WHERE id = {pk} "
-            cursor.execute(query)
-            _pk, _name, _company_name = cursor.fetchone()
-            cursor.close()
-            return Stocks(pk=_pk, name=_name, company_name=_company_name)
+            conn.cursor.execute(query)
+            pk, name, company_name = conn.cursor.fetchone()
+            return Stocks(pk=pk, name=name, company_name=company_name)
