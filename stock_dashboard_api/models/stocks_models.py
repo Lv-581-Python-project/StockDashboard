ec87219b9da43b1ab050984a1746fbaac8b65035
Nevmerzhitsky Yura <nevmerzhitsky.jyra@gmail.com>
2021-03-31 22:37:01 +0300

    Refactored stocks_models

--- stock_dashboard_api/models/stocks_models.py
+++ stock_dashboard_api/models/stocks_models.py
@@ -2,6 +2,7 @@ from stock_dashboard_api.utils import pool as db
 
 
 class Stocks:
+    _table = 'public.stocks'
 
     def __init__(self, name, company_name, pk=None):
         self.pk = pk
@@ -11,7 +12,7 @@ class Stocks:
     @classmethod
     def create(cls, name, company_name):
         with db.Connection() as conn:
-            query = """INSERT INTO public.stocks (name, company_name)
+            query = f"""INSERT INTO {cls._table} (name, company_name)
                         VALUES (%(name)s, %(company_name)s)
                         RETURNING id, name, company_name;"""
             conn.cursor.execute(query, {'name': name, 'company_name': company_name})
@@ -19,7 +20,7 @@ class Stocks:
             return Stocks(pk=pk, name=name, company_name=company_name)
 
     def update(self, name=None, company_name=None):
-        query = "UPDATE public.stocks SET "
+        query = f"UPDATE {self._table} SET "
         if name is not None:
             query += "name = %(name)s"
             query += ", " if company_name is not None else " "
@@ -35,13 +36,13 @@ class Stocks:
     @classmethod
     def delete_by_id(cls, pk):
         with db.Connection() as conn:
-            query = f"DELETE FROM public.stocks WHERE id = %(id)s;"
+            query = f"DELETE FROM {cls._table} WHERE id = %(id)s;"
             conn.cursor.execute(query, {'id': pk})
 
     @classmethod
     def get_by_id(cls, pk):
         with db.Connection() as conn:
-            query = f"SELECT id, name, company_name FROM public.stocks WHERE id = %(id)s "
+            query = f"SELECT id, name, company_name FROM {cls._table} WHERE id = %(id)s "
             conn.cursor.execute(query, {'id': pk})
             pk, name, company_name = conn.cursor.fetchone()
             return Stocks(pk=pk, name=name, company_name=company_name)
