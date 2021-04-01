740587cc0b0e2628423bf6b69a7ab7d82616b69a
OstapOmelchuk <ostap.omelchuk@gmail.com>
2021-04-01 14:56:11 +0300

    Fixed stocks_data_models.py

--- stock_dashboard_api/models/stocks_data_models.py
+++ stock_dashboard_api/models/stocks_data_models.py
@@ -14,22 +14,22 @@ class StocksData:
     def create(cls, stock_id, price, date_time):
         with db.Connection() as conn:
             query = f"""INSERT INTO {cls._table} (stock_id, price, date_time)
-                        VALUES ('{stock_id}', '{price}', '{date_time}')
+                        VALUES (%(stock_id)s, %(price)s, %(date_time)s)
                         RETURNING id, stock_id, price, date_time;"""
-            conn.cursor.execute(query)
+            conn.cursor.execute(query, {'stock_id': stock_id, 'price': price, 'date_time': date_time})
             pk, stock_id, price, date_time = conn.cursor.fetchone()
             return StocksData(pk=pk, stock_id=stock_id, price=price, date_time=date_time)
 
     def update(self, price=None, date_time=None):
         query = f"UPDATE {self._table} SET "
         if price is not None:
-            query += f"price = {price}"
+            query += f"price = %(price)s"
             query += ", " if date_time is not None else " "
         if date_time is not None:
-            query += f"date_time = {date_time} "
-        query += f"WHERE id = {self.pk} RETURNING id, stock_id, price, date_time;"
+            query += f"date_time = %(date_time)s "
+        query += f"WHERE id = %(pk)s RETURNING id, stock_id, price, date_time;"
         with db.Connection() as conn:
-            conn.cursor.execute(query)
+            conn.cursor.execute(query, {'price': price, 'date_time': date_time, "pk": self.pk})
             pk, stock_id, price, date_time = conn.cursor.fetchone()
             self.price = price
             self.date_time = date_time
