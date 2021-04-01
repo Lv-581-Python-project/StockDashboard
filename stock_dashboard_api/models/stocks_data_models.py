301db01957e1a62d2f98143b737d702d8d165231
OstapOmelchuk <ostap.omelchuk@gmail.com>
2021-04-01 15:16:01 +0300

    Changed name date_time to create_at in stocks_data_models.py

--- stock_dashboard_api/models/stocks_data_models.py
+++ stock_dashboard_api/models/stocks_data_models.py
@@ -4,43 +4,43 @@ from stock_dashboard_api.utils import pool as db
 class StocksData:
     _table = "public.stocks_data"
 
-    def __init__(self, stock_id, price, date_time, pk=None):
+    def __init__(self, stock_id, price, create_at, pk=None):
         self.pk = pk
         self.stock_id = stock_id
         self.price = price
-        self.date_time = date_time
+        self.create_at = create_at
 
     @classmethod
-    def create(cls, stock_id, price, date_time):
+    def create(cls, stock_id, price, create_at):
         with db.Connection() as conn:
-            query = f"""INSERT INTO {cls._table} (stock_id, price, date_time)
-                        VALUES (%(stock_id)s, %(price)s, %(date_time)s)
-                        RETURNING id, stock_id, price, date_time;"""
-            conn.cursor.execute(query, {'stock_id': stock_id, 'price': price, 'date_time': date_time})
-            pk, stock_id, price, date_time = conn.cursor.fetchone()
-            return StocksData(pk=pk, stock_id=stock_id, price=price, date_time=date_time)
+            query = f"""INSERT INTO {cls._table} (stock_id, price, create_at)
+                        VALUES (%(stock_id)s, %(price)s, %(create_at)s)
+                        RETURNING id, stock_id, price, create_at;"""
+            conn.cursor.execute(query, {'stock_id': stock_id, 'price': price, 'create_at': create_at.strftime("%Y-%m-%d %H:%M:%S")})
+            pk, stock_id, price, create_at = conn.cursor.fetchone()
+            return StocksData(pk=pk, stock_id=stock_id, price=price, create_at=create_at)
 
-    def update(self, price=None, date_time=None):
+    def update(self, price=None, create_at=None):
         query = f"UPDATE {self._table} SET "
         if price is not None:
             query += f"price = %(price)s"
-            query += ", " if date_time is not None else " "
-        if date_time is not None:
-            query += f"date_time = %(date_time)s "
-        query += f"WHERE id = %(pk)s RETURNING id, stock_id, price, date_time;"
+            query += ", " if create_at is not None else " "
+        if create_at is not None:
+            query += f"create_at = %(create_at)s "
+        query += f"WHERE id = %(pk)s RETURNING id, stock_id, price, create_at;"
         with db.Connection() as conn:
-            conn.cursor.execute(query, {'price': price, 'date_time': date_time, "pk": self.pk})
-            pk, stock_id, price, date_time = conn.cursor.fetchone()
+            conn.cursor.execute(query, {'price': price, 'create_at': create_at.strftime("%Y-%m-%d %H:%M:%S"), "pk": self.pk})
+            pk, stock_id, price, create_at = conn.cursor.fetchone()
             self.price = price
-            self.date_time = date_time
+            self.create_at = create_at
 
     @classmethod
     def get_by_id(cls, pk):
         with db.Connection() as conn:
-            query = f"SELECT id, stock_id, price, date_time FROM {cls._table} WHERE id = {pk};"
+            query = f"SELECT id, stock_id, price, create_at FROM {cls._table} WHERE id = {pk};"
             conn.cursor.execute(query)
-            pk, stock_id, price, date_time = conn.cursor.fetchone()
-            return StocksData(pk=pk, stock_id=stock_id, price=price, date_time=date_time)
+            pk, stock_id, price, create_at = conn.cursor.fetchone()
+            return StocksData(pk=pk, stock_id=stock_id, price=price, create_at=create_at)
 
     @classmethod
     def delete_by_id(cls, pk):
