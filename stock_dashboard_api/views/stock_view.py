f839aca2a02db3ec56b0407ac05b2160044d1c21
Daniil Oleshchuk <danieloleshuk@gmail.com>
2021-04-03 20:09:16 +0300

    added views for stock model

---
+++ stock_dashboard_api/views/stock_view.py
@@ -0,0 +1,69 @@
+import json
+
+from flask import Blueprint, request, make_response, jsonify
+from flask.views import MethodView
+
+from stock_dashboard_api.models.stock_model import Stock
+
+
+class StockView(MethodView):
+    def get(self, pk):
+        if pk:
+            stock = Stock.get_by_id(pk)
+            if stock:
+                return make_response(jsonify(stock.to_dict()), 200)
+        return make_response('Wrong data provided', 400)
+
+    def post(self):
+        try:
+            body = request.get_json()
+        except json.JSONDecodeError:
+            return make_response("Wrong data provided", 400)
+        stock_name, stock_company_name = body.get('name'), body.get('company name')
+
+        if (type(stock_name) is str) and (type(stock_company_name) is str):
+            stock_to_create = {'name': stock_name, 'company_name': stock_company_name}
+        else:
+            return make_response("Wrong data provided", 400)
+
+        stock = Stock(**stock_to_create)
+        if stock:
+            return make_response(jsonify(stock.to_dict()), 200)
+        return make_response("An error occurred during entity creating", 400)
+
+    def put(self, pk):
+        stock = Stock.get_by_id(pk)
+        if not stock:
+            return make_response("Wrong data provided", 400)
+        try:
+            body = request.get_json()
+        except json.JSONDecodeError:
+            return make_response("Wrong data provided", 400)
+        stock_name, stock_company_name = body.get('name'), body.get('company name')
+        stock_values_to_update = {}
+        if type(stock_name) is str:
+            stock_values_to_update['name'] = stock_name
+        if type(stock_company_name) is str:
+            stock_values_to_update['company_name'] = stock_company_name
+        # MODEL HAVE TO RETURN NEW VALUE
+        # ITS POSSIBLE TO ADD 'RETURNING' BLOCK IN SQL-QUERY, IN ORDER TO DO EVERYTHING AT ONCE
+        # FIXME: it will fail, if model won't return new value
+        stock = stock.update(**stock_values_to_update)
+        if stock:
+            return make_response(jsonify(stock.to_dict()), 200)
+        return make_response("An error occurred during entity updating", 400)
+
+    def delete(self, pk):
+        if Stock.get_by_id(pk):
+            # FIXME: add entity removal tracking
+            Stock.delete_by_id(pk)
+            return make_response('Removed successfully', 200)
+        return make_response("Wrong data provided", 400)
+
+
+stock_view_blueprint = Blueprint('stock', __name__, url_prefix='/stocks')
+
+stock_view = StockView.as_view('stock_view')
+
+stock_view_blueprint.add_url_rule('/', view_func=stock_view, methods=['POST', ])
+stock_view_blueprint.add_url_rule('/<int:pk>', view_func=stock_view, methods=['GET', 'PUT', 'DELETE'])
