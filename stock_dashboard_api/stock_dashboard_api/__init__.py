4cff1ce9c0c7d24a7a99827cc5d0e91a8c65706b
StepanTchynetskyi <tchynetsky@gmail.com>
2021-04-04 22:52:25 +0300

    added before request function that validates json

--- stock_dashboard_api/stock_dashboard_api/__init__.py
+++ stock_dashboard_api/stock_dashboard_api/__init__.py
@@ -5,3 +5,4 @@ from stock_dashboard_api.stock_dashboard_api.config import DevelopmentConfig
 app = Flask(__name__)
 
 app.config.from_object(DevelopmentConfig)
+from stock_dashboard_api.utils.middleware import json_middleware
