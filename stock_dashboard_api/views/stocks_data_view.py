c38cba85c24e5e0c6dc7a99ddea9579dae066fe5
StepanTchynetskyi <tchynetsky@gmail.com>
2021-04-04 20:02:44 +0300

    added tests for StockDataView and fixed StockDataView

--- stock_dashboard_api/views/stocks_data_view.py
+++ stock_dashboard_api/views/stocks_data_view.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Blueprint, request, jsonify, make_response, json
+from flask import Blueprint, request, jsonify, make_response
 from flask.views import MethodView
 from stock_dashboard_api.models.stock_data_models import StocksData
 
@@ -16,22 +16,17 @@ class StockDataView(MethodView):
         return make_response(jsonify(stock_data.to_dict()), 200)
 
     def post(self):
-
         try:
             body = json.loads(request.get_json())
 
         except (ValueError, KeyError, TypeError):
-            return make_response("Invalid JSON", 400)
-
-        if body is None:
-            return make_response("No data provided", 400)
+            return make_response("Wrong data provided", 400)
 
         data_to_create = {
             'stock_id': body.get('stock_id'),
             'price': body.get('price'),
             'create_at': body.get('create_at')
         }
-        # TODO check if stock_id exists in Stock table
         stock_data = StocksData.create(**data_to_create)
         if stock_data is None:
             return make_response("Stock data is not created", 400)
@@ -43,22 +38,25 @@ class StockDataView(MethodView):
             return make_response("Can not find stock data, wrong id", 400)
 
         try:
-            body = request.get_json()
-        except json.JSONDecodeError:
-            return make_response("Invalid JSON", 400)
+            body = json.loads(request.get_json())
+
+        except (ValueError, KeyError, TypeError):
+            return make_response("Wrong data provided", 400)
 
         data_to_update = {
             'price': body.get('price'),
             'create_at': body.get('create_at')
         }
         stock_data_updated = stock_data.update(**data_to_update)
-        # TODO check if stock_data is updated
-        return make_response(jsonify(stock_data_updated.to_dict()), 200)
-
+        if stock_data_updated:
+            return make_response(jsonify(stock_data_updated.to_dict()), 200)
+        return make_response("Stock Data is not updated, possible you input wrong data", 400)
+    
     def delete(self, pk):
         stock_data_deleted = StocksData.delete_by_id(pk=pk)
-        # TODO check if stock_data is deleted
-        return make_response(f"Stock data {pk} deleted", 200)
+        if stock_data_deleted:
+            return make_response("Stock data deleted", 200)
+        return make_response("Stock data not deleted", 400)
 
 
 stock_data_view = StockDataView.as_view('stock_data_view')
--- stock_dashboard_api/views/tests/test_stock_data_view.py
+++ stock_dashboard_api/views/tests/test_stock_data_view.py
@@ -1,48 +1,145 @@
-import pytest
-import requests
 import json
+from unittest.mock import patch
+from stock_dashboard_api.stock_dashboard_api import app
 
-def test_get_pass():
-    response = requests.get('http://127.0.0.1:5000/stocks_data/1')
-    print(response.json()['id'])
-    assert response.status_code == 200
-    assert response.json()['id'] == 1
-    assert response.json()['create_at'] == '2021-01-01'
-    assert response.json()['price'] == 300
-    assert response.json()['stock_id'] == 2
+from stock_dashboard_api.models.stock_data_models import StocksData
+
+@patch('stock_dashboard_api.models.stock_data_models.StocksData.get_by_id')
+def test_get_pass(mock_get):
+    with app.app_context():
+        mock_get.return_value = StocksData(stock_id=2, price=300, create_at="2021-01-01", pk=1)
+        with app.test_client() as client:
+            response = client.get('/stocks_data/1')
+            body = json.loads(response.data)
+            assert response.status_code == 200
+            assert body['id'] == 1
+            assert body['create_at'] == '2021-01-01'
+            assert body['price'] == 300
+            assert body['stock_id'] == 2
+
+@patch('stock_dashboard_api.models.stock_data_models.StocksData.get_by_id')
+def test_get_fail(mock_get):
+    with app.app_context():
+        mock_get.return_value = None
+        with app.test_client() as client:
+            response = client.get('/stocks_data/2')
+            assert response.status_code == 400
+            assert response.data == b"Can not find stock data, wrong id"
 
 
-def test_get_fail():
-    response = requests.get('http://127.0.0.1:5000/stocks_data/2')
-    assert response.status_code == 400
-    assert response.text == "Can not find stock data, wrong id"
+@patch('stock_dashboard_api.models.stock_data_models.StocksData.create')
+def test_post_pass(mock_post):
+    with app.app_context():
+        mock_post.return_value = StocksData(stock_id=3, price=300, create_at="2020-01-01")
+        with app.test_client() as client:
+            data = json.dumps({"stock_id": 3,
+                               "price": 300,
+                               "create_at": "2020-01-01"})
+            response = client.post('/stocks_data/', json=data)
+            body = json.loads(response.data)
+            assert response.status_code == 201
+            assert body['create_at'] == '2020-01-01'
+            assert body['price'] == 300
+            assert body['stock_id'] == 3
 
 
-def test_post_pass():
-    data = json.dumps({"stock_id": 3,
-            "price": 300,
-            "create_at": "2020-01-01"})
-    response = requests.post('http://127.0.0.1:5000/stocks_data/', json=data)
-    assert response.status_code == 201
-    assert response.json()['id'] == 1
-    assert response.json()['create_at'] == '2020-01-01'
-    assert response.json()['price'] == 300
-    assert response.json()['stock_id'] == 3
+def test_post_data_fail():
+    with app.test_client() as client:
+        incorrect_json = '{ "stock_id:"12 "price":30 "create_at:"None" }'
+        response = client.post('/stocks_data/', json=incorrect_json)
+        assert response.status_code == 400
+        assert response.data == b"Wrong data provided"
+        response = client.post('/stocks_data/')
 
-def test_post_fail():
-    incorrect_json = '{ name":"John "age":30 "car:"None" }'
-    response = requests.post('http://127.0.0.1:5000/stocks_data/', json=incorrect_json)
-    assert response.status_code == 400
-    assert response.text == "Invalid JSON"
+        assert response.status_code == 400
+        assert response.data == b"Wrong data provided"
 
-    response = requests.post('http://127.0.0.1:5000/stocks_data/')
-    assert response.status_code == 400
-    assert response.text == "No data provided"
 
-    data = json.dumps({"stock_id": 3,
+@patch('stock_dashboard_api.models.stock_data_models.StocksData.create')
+def test_post_create_fail(mock_post):
+    with app.app_context():
+        mock_post.return_value = None
+        with app.test_client() as client:
+            data = json.dumps({"stock_id": 3,
                        "price": 300
                        })
-    response = requests.post('http://127.0.0.1:5000/stocks_data/',json=data)
-    assert response.status_code == 400
-    assert response.text == "Stock data is not created"
+            response = client.post('/stocks_data/', json=data)
+            assert response.status_code == 400
+            assert response.data == b"Stock data is not created"
 
+
+@patch('stock_dashboard_api.models.stock_data_models.StocksData.get_by_id')
+@patch('stock_dashboard_api.models.stock_data_models.StocksData.update')
+def test_put_success(mock_put, mock_get):
+    with app.app_context():
+        mock_get.return_value = StocksData(stock_id=2, price=300, create_at="2021-01-01", pk=1)
+        mock_put.return_value = StocksData(stock_id=2, price=500, create_at="2021-01-08", pk=1)
+        with app.test_client() as client:
+            data = json.dumps({"price": 500,
+                               "create_at": "2021-01-08"
+                               })
+            response = client.put('/stocks_data/1', json=data)
+            body = json.loads(response.data)
+            assert response.status_code == 200
+            assert body['id'] == 1
+            assert body['create_at'] == '2021-01-08'
+            assert body['price'] == 500
+            assert body['stock_id'] == 2
+
+
+@patch('stock_dashboard_api.models.stock_data_models.StocksData.get_by_id')
+def test_put_not_existing_pk(mock_get):
+    with app.app_context():
+        mock_get.return_value = None
+        with app.test_client() as client:
+            data = json.dumps({"price": 500,
+                               "create_at": "2021-01-08"
+                               })
+            response = client.put('/stocks_data/2', json=data)
+            assert response.status_code == 400
+            assert response.data == b"Can not find stock data, wrong id"
+
+
+@patch('stock_dashboard_api.models.stock_data_models.StocksData.get_by_id')
+def test_put_wrong_json(mock_get):
+    with app.app_context():
+        mock_get.return_value = StocksData(stock_id=2, price=300, create_at="2021-01-01", pk=1)
+        with app.test_client() as client:
+            incorrect_json = '{ "stock_id:"12 "price":30 "create_at:"None" }'
+            response = client.put('/stocks_data/2', json=incorrect_json)
+            assert response.status_code == 400
+            assert response.data == b"Wrong data provided"
+
+
+@patch('stock_dashboard_api.models.stock_data_models.StocksData.get_by_id')
+@patch('stock_dashboard_api.models.stock_data_models.StocksData.update')
+def test_put_wrong_values(mock_put, mock_get):
+    with app.app_context():
+        mock_get.return_value = StocksData(stock_id=2, price=300, create_at="2021-01-01", pk=1)
+        mock_put.return_value = None
+        with app.test_client() as client:
+            data = json.dumps({"price": "ajif",
+                               "create_at": "2021-01-08"
+                               })
+            response = client.put('/stocks_data/2', json=data)
+            assert response.status_code == 400
+            assert response.data == b"Stock Data is not updated, possible you input wrong data"
+
+
+@patch('stock_dashboard_api.models.stock_data_models.StocksData.delete_by_id')
+def test_delete_success(mock_delete):
+    with app.app_context():
+        mock_delete.return_value = True
+        with app.test_client() as client:
+            response = client.delete('/stocks_data/1')
+            assert response.status_code == 200
+            assert response.data == b"Stock data deleted"
+
+@patch('stock_dashboard_api.models.stock_data_models.StocksData.delete_by_id')
+def test_delete_fail(mock_delete):
+    with app.app_context():
+        mock_delete.return_value = None
+        with app.test_client() as client:
+            response = client.delete('/stocks_data/1')
+            assert response.status_code == 400
+            assert response.data == b"Stock data not deleted"
