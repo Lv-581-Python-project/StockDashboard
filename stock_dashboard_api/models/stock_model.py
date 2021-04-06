803a4369c382c8755af9e0dbb6b510a38f1315a0
Nevmerzhitsky Yura <nevmerzhitsky.jyra@gmail.com>
2021-04-06 13:35:35 +0300

    Refactored stok_model

--- stock_dashboard_api/models/stock_model.py
+++ stock_dashboard_api/models/stock_model.py
@@ -15,9 +15,12 @@ class Stock:
             query = f"""INSERT INTO {cls._table} (name, company_name)
                         VALUES (%(name)s, %(company_name)s)
                         RETURNING id, name, company_name;"""
-            conn.cursor.execute(query, {'name': name, 'company_name': company_name})
-            pk, name, company_name = conn.cursor.fetchone()
-            return Stock(pk=pk, name=name, company_name=company_name).to_dict()
+            try:
+                conn.cursor.execute(query, {'name': name, 'company_name': company_name})
+                pk, name, company_name = conn.cursor.fetchone()
+                return Stock(pk=pk, name=name, company_name=company_name)
+            except:
+                return False
 
     def update(self, name=None, company_name=None):
         list_with_variable = []
@@ -28,26 +31,38 @@ class Stock:
         query = f"""UPDATE {self._table} SET {', '.join(list_with_variable)}
                 WHERE id = %(pk)s RETURNING id, name, company_name; """
         with pool_manager() as conn:
-            conn.cursor.execute(
-                query,
-                {'name': name, 'company_name': company_name, 'pk': self.pk})
-            pk, name, company_name = conn.cursor.fetchone()
-            self.name = name
-            self.company_name = company_name
+            try:
+                conn.cursor.execute(
+                    query,
+                    {'name': name, 'company_name': company_name, 'pk': self.pk})
+                pk, name, company_name = conn.cursor.fetchone()
+                self.name = name
+                self.company_name = company_name
+                return True
+            except:
+                return False
+
 
     @classmethod
     def delete_by_id(cls, pk):
         with pool_manager() as conn:
             query = f"DELETE FROM {cls._table} WHERE id = %(id)s;"
-            conn.cursor.execute(query, {'id': pk})
+            try:
+                conn.cursor.execute(query, {'id': pk})
+                return True
+            except:
+                return False
 
     @classmethod
     def get_by_id(cls, pk):
         with pool_manager() as conn:
             query = f"SELECT id, name, company_name FROM {cls._table} WHERE id = %(id)s"
-            conn.cursor.execute(query, {'id': pk})
-            pk, name, company_name = conn.cursor.fetchone()
-            return Stock(pk=pk, name=name, company_name=company_name).to_dict()
+            try:
+                conn.cursor.execute(query, {'id': pk})
+                pk, name, company_name = conn.cursor.fetchone()
+                return Stock(pk=pk, name=name, company_name=company_name)
+            except:
+                return None
 
     def to_dict(self):
         return {'id': self.pk, "name": self.name, "company_name": self.company_name}
