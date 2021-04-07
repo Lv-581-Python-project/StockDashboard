4e34803427f72c7e1c9ca56d6d0524621d488a1c
Nevmerzhitsky Yura <nevmerzhitsky.jyra@gmail.com>
2021-04-07 14:06:00 +0300

    Test for stock_model

--- tests/stock_dashboard_api/models/test_stock_model.py
+++ tests/stock_dashboard_api/models/test_stock_model.py
@@ -2,18 +2,32 @@ import unittest
 from unittest.mock import patch
 from stock_dashboard_api.models import stock_model as sm
 
-@patch('stock_dashboard_api.models.stocks_models.pool_manager', **{'method.return_value': 3})
+inst = sm.Stock(1,'2323','2323')
+
+
+@patch('stock_dashboard_api.models.stock_model.pool_manager')
 class TestStock(unittest.TestCase):
-    def test_create(self, pool_manager):
+
+    def test_create_true(self, pool_manager):
         data = {"id": 1, "name": "AAPL", "company_name": "Apple"}
         pool_manager.return_value.__enter__.return_value.cursor.fetchone.return_value = (1,"AAPL", "Apple")
+        self.assertEqual(sm.Stock.create("AAPL", "Apple").to_dict(), data)
 
-        self.assertEqual(sm.Stock.create("AAPL", "Apple"), data)
 
     def test_update(self, pool_manager):
+        pool_manager.return_value.__enter__.return_value.cursor.fetchone.return_value = (1,"AAPL", "Apple")
+        pool_manager.return_value.__exit__.return_value= True
+        self.assertEqual(sm.Stock(1,'IBM','IBM').update(name="AAPL", company_name="Apple"), True)
+
+
+    def test_delete(self, pool_manager):
+        pool_manager.return_value.__enter__.return_value.__exit__.return_value = True
+        self.assertEqual(sm.Stock.delete_by_id(1), True)
+
+    def test_get_by_id(self, pool_manager):
         data = {"id": 1, "name": "AAPL", "company_name": "Apple"}
-        pool_manager.return_value.__enter__.return_value.cursor.fetchone.return_value = (1, "AAPL", "Apple")
-        self.assertEqual(sm.Stock.update("AAPL", "Apple"), data)
+        pool_manager.return_value.__enter__.return_value.cursor.fetchone.return_value = (1,"AAPL", "Apple")
+        self.assertEqual(sm.Stock.get_by_id(1).to_dict(), data)
 
 
 
