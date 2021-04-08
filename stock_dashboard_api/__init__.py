917bcba4f2eb9ee7ed19bc338b6947ba89c20ebc
Andrii Romaniuk <marioboy@quintagroup.org>
2021-04-08 18:25:17 +0300

    Config views fixes, model method fixes feat Stepan

--- stock_dashboard_api/__init__.py
+++ stock_dashboard_api/__init__.py
@@ -4,7 +4,7 @@ import os
 from flask import Flask, request, make_response
 
 from stock_dashboard_api.config import DevelopmentConfig
-from stock_dashboard_api.views import stock_view, dashboard_views, stocks_data_view
+from stock_dashboard_api.views import stock_view, dashboard_views, stocks_data_view, stock_config_view
 
 TEMPLATE_FOLDER = os.path.join(os.environ.get('PROD_ROOT'), 'templates')
 app = Flask(__name__, template_folder=TEMPLATE_FOLDER)
@@ -14,13 +14,14 @@ app.config.from_object(DevelopmentConfig)
 app.register_blueprint(stock_view.mod)
 app.register_blueprint(stocks_data_view.mod)
 app.register_blueprint(dashboard_views.mod)
+app.register_blueprint(stock_config_view.mod)
 
 PATH_PATTERNS_BODY_PARSE_JSON = ['/stocks_data/', '/stocks/', '/stock_conf/']
 
 
 @app.before_request
 def middleware_body_parse_json():
-    """A function that appears before request,
+    """A function that executes before request,
     receive and validate json data for PUT and POST request methods.
     """
     current_path = request.path
