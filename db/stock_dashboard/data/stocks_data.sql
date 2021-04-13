f565dba40665e773d1c8730bf93901de223626ff
SLDem <slavko.dem@gmail.com>
2021-04-13 15:09:33 +0300

    Changed create_at to created_at in the database

--- db/stock_dashboard/data/stocks_data.sql
+++ db/stock_dashboard/data/stocks_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO stocks_data(stock_id, price, create_at) VALUES
+INSERT INTO stocks_data(stock_id, price, created_at) VALUES
     (1, 144.15, '2020-04-01 05:21:22'),
     (2, 138.78, '2020-01-09 03:41:13'),
     (3, 187.25, '2020-05-11 03:55:14');
