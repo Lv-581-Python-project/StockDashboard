4fb203606102debf7a7026bcde8ac9e53db08054
Nevmerzhitsky Yura <nevmerzhitsky.jyra@gmail.com>
2021-04-18 08:25:15 +0300

    Create yahoo_finance util

---
+++ workers/utils/yahoo_finance.py
@@ -0,0 +1,33 @@
+import yfinance as yf
+
+from stock_dashboard_api.utils.pool import pool_manager
+
+
+def check_if_exist(ticket):
+    all_info = yf.Ticker(ticket)
+    if len(all_info.info) == 1:
+        raise False
+    return True
+
+
+def get_one_ticket(ticket):
+    if check_if_exist(ticket):
+        all_info = yf.Ticker(ticket)
+        name = all_info.info['symbol']
+        company_name = all_info.info['shortName']
+        ticket_id_in_db = None
+        with pool_manager() as conn:
+            query = 'INSERT INTO public.stocks(name, company_name) VALUES (%(name)s, %(company_name)s);'
+            conn.cursor.execute(query, {'name': name, 'company_name': company_name})
+
+            query = 'SELECT id FROM public.stocks WHERE name = %(name)s'
+            conn.cursor.execute(query, {'name': name})
+            ticket_id_in_db = conn.cursor.fetchone()
+
+            data = all_info.history('5d', interval='15m')
+
+            query = 'INSERT INTO public.stocks_data(stock_id, price, create_at) ' \
+                    'VALUES (%(stock_id)s, %(price)s, %(create_at)s)'
+            for raw in data.itertuples():
+                conn.cursor.execute(query, {'stock_id': ticket_id_in_db, 'price': raw[2],
+                                            'create_at': raw[0].astimezone(tz=None)})
