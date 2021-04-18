c907c1d71e71b06b2bbaad30f6148caae9560754
Daniil Oleshchuk <danieloleshuk@gmail.com>
2021-04-18 19:23:16 +0300

    removed react template integration to flask

--- stock_dashboard_api/__init__.py
+++ stock_dashboard_api/__init__.py
@@ -7,11 +7,10 @@ from stock_dashboard_api.config import DevelopmentConfig
 from stock_dashboard_api.views import stock_view, dashboard_views, stocks_data_view, stock_config_view
 
 
-TEMPLATE_ROOT = os.environ.get('TEMPLATE_ROOT')
-TEMPLATE_FOLDER = os.path.join(TEMPLATE_ROOT, 'build')
-STATIC_FOLDER = os.path.join(TEMPLATE_ROOT, 'build/static')
+TEMPLATE_FOLDER = os.path.join(os.environ.get('PROD_ROOT'), 'templates')
+app = Flask(__name__, template_folder=TEMPLATE_FOLDER)
 
-app = Flask(__name__, template_folder=TEMPLATE_FOLDER, static_folder=STATIC_FOLDER)
+app.config.from_object(DevelopmentConfig())
 
 app.config.from_object(DevelopmentConfig())
 
