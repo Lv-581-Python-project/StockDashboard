c5bd38eedfe417c052b76f3544c1a67435185fdb
titov <titov@ucu.edu.ua>
2021-04-22 16:36:48 +0300

    Change schema for stocks and improve insert

--- db/stock_dashboard/schema/stocks.sql
+++ db/stock_dashboard/schema/stocks.sql
@@ -1,6 +1,9 @@
 CREATE TABLE IF NOT EXISTS stocks(
     id serial PRIMARY KEY,
     name VARCHAR(32) NOT NULL,
-    company_name VARCHAR(128) NOT NULL,
+    company_name VARCHAR(256) NOT NULL,
+    country VARCHAR(32) NOT NULL,
+    industry VARCHAR(128) NOT NULL,
+    sector VARCHAR(32) NOT NULL,
     in_use BOOLEAN NOT NULL DEFAULT FALSE
 );
