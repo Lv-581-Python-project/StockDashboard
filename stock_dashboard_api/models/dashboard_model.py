afe434bcef1bfac27b8017f5c523ef237ddc391c
StepanTchynetskyi <tchynetsky@gmail.com>
2021-04-22 16:07:32 +0300

    modified dashboard_model,dashboard_view methods and config_hash_generator methods, added Docstring

--- stock_dashboard_api/models/dashboard_model.py
+++ stock_dashboard_api/models/dashboard_model.py
@@ -1,11 +1,12 @@
 import psycopg2
 
+from stock_dashboard_api.utils.config_hash_generator import generate_uuid
 from stock_dashboard_api.utils.pool import pool_manager
 
 
 class Dashboard:
     """
-    Model used to create a dashboard_config.
+    Model used to create a Dashboard.
     """
 
     _table = 'public.dashboard'
@@ -15,37 +16,38 @@ class Dashboard:
         self.config_hash = config_hash
 
     @classmethod
-    def create(cls, config_hash: str, stocks=None) -> object:
+    def create(cls, config_hash=None, stocks=None) -> object:
+        """Creates a new record in Dashboard table
+
+        :param config_hash: specific config hash for record
+        :param stocks: list of stocks for specific dashboard
+        :return: instance of Dashboard
         """
-        Creates a new dashboard_config.
-        :return: instance of DashboardConfig
-        """
+
+        if not config_hash:
+            all_config_hashes = Dashboard.get_all_hashes()
+            config_hash = generate_uuid(all_config_hashes)
         with pool_manager() as conn:
-            _dashboard_has_stocks_table = 'public.dashboard_has_stocks'
-            dashboard_insert_query = f"""INSERT INTO {cls._table} (config_hash)
+            dashboard_has_stocks_table = 'public.dashboard_has_stocks'
+            insert_dashboard_query = f"""INSERT INTO {cls._table} (config_hash)
                         VALUES (%(config_hash)s)
                         RETURNING id, config_hash;"""
-
             try:
-                conn.cursor.execute(dashboard_insert_query, {'config_hash': config_hash})
+                conn.cursor.execute(insert_dashboard_query, {'config_hash': config_hash})
                 pk, config_hash = conn.cursor.fetchone()  # pylint: disable=C0103,  W0613
                 if stocks:
-                    for stock in stocks:
-                        dashboard_has_stocks_query = (f"INSERT INTO {_dashboard_has_stocks_table} "
-                                                      "(stock_id, dashboard_id, datetime_from, datetime_to)"
-                                                      " VALUES (%(stock_id)s, %(dashboard_id)s,"
-                                                      " %(datetime_from)s, %(datetime_to)s);")
-                        conn.cursor.execute(dashboard_has_stocks_query,
-                                            {"stock_id": stock["stock_id"], "dashboard_id": pk,
-                                             "datetime_from": stock["datetime_from"],
-                                             "datetime_to": stock["datetime_to"]})
+                    stocks = [(stock["stock_id"], pk, stock["datetime_from"], stock["datetime_to"]) for stock in stocks]
+                    insert_dashboard_has_stocks_query = (f"INSERT INTO {dashboard_has_stocks_table} "
+                                                         "(stock_id, dashboard_id, datetime_from, datetime_to)"
+                                                         " VALUES (%s, %s, %s, %s);")
+                    conn.cursor.executemany(insert_dashboard_has_stocks_query, stocks)
             except (psycopg2.ProgrammingError, psycopg2.DatabaseError) as err:
                 return False
             return Dashboard(pk=pk, config_hash=config_hash)
 
     def update(self, config_hash: str) -> bool:
         """
-        Updates an existing dashboard_config.
+        Updates an existing dashboard.
         """
         if not config_hash:
             return False
@@ -63,7 +65,7 @@ class Dashboard:
     @classmethod
     def get_by_id(cls, pk: int) -> object:  # pylint: disable=C0103,  W0613
         """
-        Returns a dashboard_config instance by its id.
+        Returns a Dashboard instance by its id.
         :return: instance of DashboardConfig model
         """
 
@@ -78,8 +80,8 @@ class Dashboard:
 
     @classmethod
     def get_by_hash(cls, config_hash: str):
-        """
-        Returns a dashboard by its id.
+        """Getting a dashboard by its hash from Dashboard table
+
         :return: instance of DashboardConfig model
         """
         with pool_manager() as conn:
@@ -90,17 +92,31 @@ class Dashboard:
                 pk = conn.cursor.fetchone()[0]
                 return Dashboard(pk=pk, config_hash=config_hash)
             except (psycopg2.ProgrammingError, psycopg2.DatabaseError, TypeError) as err:
+                return None
+            
+    @classmethod
+    def get_all_hashes(cls) -> list:
+        """
+        Getting all config hashes from Dashboard table
+        :return: list of dashboard config hashes
+        """
+        with pool_manager() as conn:
+            query = f"""SELECT config_hash FROM {cls._table};"""
+            try:
+                conn.cursor.execute(query)
 
+                all_config_hashes = conn.cursor.fetchall()
+                all_config_hashes = [config_hash[0] for config_hash in all_config_hashes]
+                return all_config_hashes
+            except (psycopg2.ProgrammingError, psycopg2.DatabaseError) as err:
                 return None
 
-    def get_stocks_by_dashboard_id(self):
-        _dashboard_has_stocks_table = 'public.dashboard_has_stocks'
+    def get_stocks(self):
+        dashboard_has_stocks_table = 'public.dashboard_has_stocks'
         with pool_manager() as conn:
             get_stocks_id_query = f"""SELECT stock_id
-                                      FROM {_dashboard_has_stocks_table}
-                                      JOIN {self._table}
-                                      ON {_dashboard_has_stocks_table}.dashboard_id={self._table}.id
-                                      WHERE {_dashboard_has_stocks_table}.dashboard_id=%(id)s;"""
+                                      FROM {dashboard_has_stocks_table}
+                                      WHERE {dashboard_has_stocks_table}.dashboard_id=%(id)s;"""
             try:
                 conn.cursor.execute(get_stocks_id_query, {'id': self.pk})
                 list_of_stocks = conn.cursor.fetchall()
--- stock_dashboard_api/views/dashboard_view.py
+++ stock_dashboard_api/views/dashboard_view.py
@@ -3,31 +3,42 @@ from flask import Blueprint, request, jsonify, make_response, Response
 from stock_dashboard_api.utils.logger import views_logger as logger
 from stock_dashboard_api.models.dashboard_model import Dashboard
 from stock_dashboard_api.utils.json_parser import middleware_body_parse_json
-from stock_dashboard_api.utils.config_hash_generator import generate_uuid
 
 mod = Blueprint('dashboard', __name__, url_prefix='/api/dashboard')
 
 
 class DashboardView(MethodView):
+    """A DashboardView is a class-based view that inherits flask view - MethodView.
+    This class implements get and post methods to handle
+    ``GET``, ``POST`` requests accordingly.
+    """
 
     def get(self, config_hash: str) -> Response:
+        """A get method is used to send a specific GET request to access Dashboard by config_hash
+
+        :param config_hash: Dashboard unique field
+        :return: a Response object with specific data and status code
+        """
         dashboard = Dashboard.get_by_hash(config_hash=config_hash)
         if dashboard is None:
             message = "Can not find dashboard, wrong hash"
             logger.info(message)
             return make_response(message, 400)
-        stocks = dashboard.get_stocks_by_dashboard_id()
+        stocks = dashboard.get_stocks()
         if stocks is None:
             return make_response("Can not find any stocks in dashboard", 400)
         return make_response(jsonify({"stocks": stocks}), 200)
 
-    def post(self):
+    def post(self) -> Response:
+        """A post method is used to send a specific POST request to create Dashboard
+
+        :return: a Response object with specific data and status code
+        """
         response = middleware_body_parse_json(request)
         if not response:
             return make_response("Wrong data provided", 400)
         stocks = request.body.get('stocks')
-        config_hash = generate_uuid()
-        dashboard = Dashboard.create(config_hash=config_hash, stocks=stocks)
+        dashboard = Dashboard.create(stocks=stocks)
         if dashboard:
             return make_response(jsonify(dashboard.to_dict()), 201)
         return make_response("Can not create a dashboard", 400)
