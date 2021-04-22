4cb6b904eeb6d3f5be180f2687b9365fc54b7bf3
Stepan Tchynetskyi <60452439+StepanTchynetskyi@users.noreply.github.com>
2021-04-22 16:55:33 +0300

    Update config_hash_generator.py

--- stock_dashboard_api/utils/config_hash_generator.py
+++ stock_dashboard_api/utils/config_hash_generator.py
@@ -5,7 +5,7 @@ def generate_uuid(all_config_hashes: list) -> str:
     """Generating unique config hash with 8 characters
 
     :param all_config_hashes: list of all hashes in Dashboard table
-    :return: unique config hash with 8 characters
+    :return: unique config hash
     """
     config_hash = str(uuid.uuid4())[:8]
     while config_hash in all_config_hashes:
