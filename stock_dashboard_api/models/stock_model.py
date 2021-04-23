c45639607039bb4dbec7baf6a873820e570636f9
OstapOmelchuk <75321892+OstapOmelchuk@users.noreply.github.com>
2021-04-23 03:50:17 +0300

    Update stock_model.py

--- stock_dashboard_api/models/stock_model.py
+++ stock_dashboard_api/models/stock_model.py
@@ -139,31 +139,6 @@ class Stock:
                                           for pk, stock_id, price, created_at in stock_data_for_time_period]
         return stock_data_for_time_period
 
-    # @classmethod
-    # def get_data_for_last_day(cls, pk: int) -> list:
-    #     stock_data_for_last_day = []
-    #     datetime_now = datetime.now().strftime(DATETIME_PATTERN)
-    #     datetime_yesterday = (datetime.now() - timedelta(days=1)).strftime(DATETIME_PATTERN)
-    #     with pool_manager() as conn:
-    #         query = """SELECT * FROM stocks_data
-    #                     WHERE stock_id = %(stock_id)s
-    #                     AND %(yesterday)s <= created_at AND created_at < %(today)s
-    #                     ORDER BY created_at;"""
-    #         try:
-    #             if Stock.get_by_id(pk):
-    #                 conn.cursor.execute(query, {'stock_id': pk,
-    #                                             'yesterday': datetime_yesterday,
-    #                                             'today': datetime_now})
-    #                 stock_data_for_last_day = conn.cursor.fetchall()
-    #             else:
-    #                 pass
-    #         except (psycopg2.DataError, psycopg2.ProgrammingError, TypeError) as e:
-    #             print(e)
-    #             pass
-    #     stock_data_for_last_day = [StockData(pk=pk, stock_id=stock_id, price=price, created_at=created_at)
-    #                                          for pk, stock_id, price, created_at in stock_data_for_last_day]
-    #     return stock_data_for_last_day
-
     @classmethod
     def get_data_for_last_day(cls, pk: int) -> list:
         stock_data_for_last_day = []
