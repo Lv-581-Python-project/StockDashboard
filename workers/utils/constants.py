7fe65a372d925c86bbda7bf861ed0edf4ff97ee5
titov <titov@ucu.edu.ua>
2021-05-17 20:31:33 +0300

    Fix TYPO

--- workers/utils/constants.py
+++ workers/utils/constants.py
@@ -1,3 +1,3 @@
 FETCH_DATA_FOR_PERIOD_TASK = 'fetch data period'
 FETCH_NEW_STOCK_TASK = 'fetch new stock'
-FETCH_HISTORICAL_DATA_TASK = 'fetch historical data’'
+FETCH_HISTORICAL_DATA_TASK = 'fetch historical data'
