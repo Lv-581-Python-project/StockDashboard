8f835c0017228fed9712c4276d4986e356b3bb37
titov <titov@ucu.edu.ua>
2021-06-10 17:49:01 +0300

    add user data schema

---
+++ db/stock_dashboard/schema/user_data.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS dashboard_has_stocks(
+    user_id INTEGER NOT NULL ,
+    username VARCHAR(64) NOT NULL,
+    email VARCHAR(32) NOT NULL,
+    password VARCHAR(32) NOT NULL
+);
